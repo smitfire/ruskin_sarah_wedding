8125a49dd5c94e624a067bfb912dd3536e53735c
Nick Smit <nick@g3s.ch>
2014-04-07 16:00:24 +0200

    initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RuskinSarahWedding::Application.config.secret_key_base = '2045b4d3c2361103577ef7e7effa31decc5116db2d9f7e0796c4bb5da80da3e6300867836591868e03b093c1b32b9795eb0456e41622bd22fda697ea86338be2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RuskinSarahWedding::Application.config.session_store :cookie_store, key: '_ruskin_sarah_wedding_session'
